3a55bc90665cc1819833180413fe51c4ef02b72d
avi <hi@avi.im>
2015-06-28 15:15:00 +0530

    added requirements file and praw as installation dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 version = '0.1'
 
 setup(
     name='praw-oauth2util',
     version=version,
+    install_requires=requirements,
     author='Benjamin Schmid',
     packages=find_packages(),
     include_package_data=True,
