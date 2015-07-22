10239c9f31d8499eedaff00a878f28a1c363c998
SmBe19 <bsgame27@gmail.com>
2015-07-22 20:40:54 +0200

    add __future__ import for python2

--- OAuth2Util/Demo.py
+++ OAuth2Util/Demo.py
@@ -6,7 +6,7 @@ import OAuth2Util
 r = praw.Reddit("OAuth2Util Demo by /u/SmBe19")
 o = OAuth2Util.OAuth2Util(r, print_log=True)
 
-o.refresh()
+o.refresh(force=True)
 
 print("Hi, {0}, you have {1} comment karma!".format(
     r.get_me().name, r.get_me().comment_karma))
--- OAuth2Util/OAuth2Util.py
+++ OAuth2Util/OAuth2Util.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 import praw
 import os
 import re
