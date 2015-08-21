f4d8b1412f59fe16da76ce39ab10ddee6d9c1d8f
SmBe19 <bsgame27@gmail.com>
2015-08-22 01:59:54 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-version = '0.2.2'
+version = '0.3.0'
 
 setup(
     name='praw-oauth2util',
